07d7ae67647ee93a766fb093f75bc31834cbc580
Jim Thoburn <jim@jimthoburn.com>
2021-11-20 18:02:29 -0800

    Add gift guide from archive.org
    https://web.archive.org/web/20211117013320/https://la2050.org/blog/2021-la2050-holiday-gift-guide

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source "https://rubygems.org"
+
+# https://github.com/rbenv/rbenv
+#
+# To upgrade to the latest rbenv and update ruby-build with newly released
+# Ruby versions, upgrade the Homebrew packages:
+#
+# $ brew upgrade rbenv ruby-build
+#
+# install a Ruby version:
+#
+# $ rbenv install 3.0.0
+#
+# Set a Ruby version to finish installation and start using commands
+#
+# $ rbenv local 3.0.0
+
+# Latest available Ruby version on Netlify
+ruby "2.7.1"
+
+gem "jekyll", "~> 4.2"
+
+# https://github.com/jekyll/jekyll/issues/8523
+gem "webrick", "~> 1.7"
---
+++ blog/2021-la2050-holiday-gift-guide/LA2050_files/la2050.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+// default js
+
+}
+/*
+     FILE ARCHIVED ON 21:02:22 Nov 07, 2021 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 01:48:34 Nov 21, 2021.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots.policy: 0.333
+  exclusion.robots: 0.357
+  load_resource: 118.096
+  esindex: 0.028
+  PetaboxLoader3.datanode: 135.202 (4)
+  LoadShardBlock: 89.435 (3)
+  CDXLines.iter: 27.007 (3)
+  PetaboxLoader3.resolve: 64.235
+  cdx.remote: 0.186
+  captures_list: 159.225
+*/
